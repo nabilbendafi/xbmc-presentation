21040039941374e22e46f36c0c6b15ddfc819646
Nabil Bendafi <nabil@bendafi.fr>
2015-09-27 13:07:00 +0200

    Dialog body

--- script.hello-world/addon.py
+++ script.hello-world/addon.py
@@ -1,11 +1,16 @@
 import xbmc
 from xbmcgui import Dialog
+from xbmcaddon import Addon
 
 if __name__ == '__main__':
     #Rock'n'Roll
     # print on XBMC log file
     xbmc.log('Hello world!')
     
+    # fetch add-on description
+    addon = Addon()
+    description = addon.getAddonInfo('description')
+
     # print on dialog box
     dialog = Dialog()
-    dialog.ok('Hello world!', 'Hello world!')
+    dialog.ok('Hello world!', description)
