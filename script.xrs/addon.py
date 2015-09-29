38758423dec946afd78386c2295ea3cbc99cc32b
Nabil Bendafi <nabil@bendafi.fr>
2015-09-29 23:09:13 +0200

    Pydev

--- script.xrs/addon.py
+++ script.xrs/addon.py
@@ -2,6 +2,24 @@ import xbmc
 from xbmcgui import Dialog
 from xbmcaddon import Addon
 
+
+addon = Addon()
+debug = addon.getSetting('debug') == 'true'
+debug_host = addon.getSetting('debug_host')
+debug_port = int(addon.getSetting('debug_port'))
+# Remote debugger using Eclipse and Pydev
+if debug:
+    try:
+        import pydevd
+        pydevd.settrace(host=debug_host, port=debug_port,
+                        stdoutToServer=True, stderrToServer=True)
+        xbmc.log('Pydev started!')
+    except ImportError:
+        xbmc.log('Pydev not found!', xbmc.LOGERROR)
+    except:
+        xbmc.log('Couldn\'t attach Pydev!', xbmc.LOGERROR)
+
+
 if __name__ == '__main__':
     #Rock'n'Roll
     pass
