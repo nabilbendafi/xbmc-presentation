8e2ee6a0a0bec0e345b16873e1a6ae5f5fd8ecae
Nabil Bendafi <nabil@bendafi.fr>
2015-10-06 14:57:59 +0200

    Main window

--- script.xrs/addon.py
+++ script.xrs/addon.py
@@ -1,6 +1,6 @@
 import xbmc
-from xbmcgui import Dialog
 from xbmcaddon import Addon
+import pyxbmct.addonwindow as pyxbmct
 
 
 addon = Addon()
@@ -20,6 +20,46 @@ if debug:
         xbmc.log('Couldn\'t attach Pydev!', xbmc.LOGERROR)
 
 
+class XRSWindow(pyxbmct.AddonFullWindow):
+
+    """Main application window."""
+
+    def __init__(self, title=''):
+        """Constructor."""
+        super(XRSWindow, self).__init__(title)
+        self.width = 850
+        """window width"""
+        self.height = 600
+        """window width"""
+        self.rows = 12
+        """grid rows"""
+        self.columns = 8
+        """grid columns"""
+        self.close_button = pyxbmct.Button('Close')
+        """close button"""
+
+        self.setGeometry(self.width, self.height, self.rows, self.columns)
+        self.draw_layout()
+        self.connect_controls()
+
+    def draw_layout(self):
+        """Draw elements on the grid."""
+        # place the close button on the grid.
+        self.placeControl(self.close_button, 11, 3, columnspan=2)
+
+    def connect_controls(self):
+        """Register actions."""
+
+        # Connect close button and BACK to close() function.
+        self.connect(self.close_button, self.close)
+        self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
+
+
 if __name__ == '__main__':
-    #Rock'n'Roll
-    pass
+    # Rock'n'Roll
+    window = XRSWindow(title='XBMC RSS Stalker')
+    window.show()
+    # display window until close() is called
+    window.doModal()
+    # xbmcgui classes are not grabage-collected, delete manually
+    del window
